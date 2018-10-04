7c92bfd43cecedb3a6802831a0d4ff18f62db9d8
itaewon <hep@hani.co.kr>
2018-10-04 18:36:08 +0900

    ~ issue 4 save

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		secretMap(5, new int[] {9, 20, 28, 18, 11}, new int[] {30, 1, 21, 17, 28});
+//		secretMap(5, new int[] {9, 20, 28, 18, 11}, new int[] {30, 1, 21, 17, 28});
 		
-		dart("1D#2S*3S");
+//		dart("1D#2S*3S");
 		
-		cache(0, new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"});
+//		cache(0, new String[] {"Jeju", "Pangyo", "Seoul", "NewYork", "LA"});
+		
+//		bus(1, 1, 5, new String[] {"08:00", "08:01", "08:02", "08:03"});
+		bus(2, 10, 2, new String[] {"09:10", "09:09", "08:00"});
 		
 	}
 	
@@ -71,6 +79,70 @@ public class Main {
 				lru.set(cities[i], cities[i] + " restaurant");
 		}
 		System.out.println(time);
+	}
+	
+	// Bus
+	public static void bus(int n, int t, int m, String[] timetable) {
+		
+		// 시작 시간, 배차 회수, 배찬 간격을 기준으로 버스들을 목록화  
+		List<LocalTime> buses = new ArrayList<LocalTime>();
+		LocalTime localTime = LocalTime.parse("09:00");
+		for (int i = 0; i < n; i++) {
+			buses.add(localTime);
+			localTime = localTime.plusMinutes(t);
+		}
+		
+		for (int i = 0; i < buses.size(); i++) {
+			System.out.println(buses.get(i));
+		}
+		System.out.println("-------------------------");
+		
+		List<LocalTime> crews = new ArrayList<LocalTime>();
+		for (int i = 0; i < timetable.length; i++) {
+			crews.add(LocalTime.parse(timetable[i]));
+		}
+		crews.sort(Comparator.comparing(LocalTime::getHour).thenComparing(Comparator.comparing(LocalTime::getMinute)));
+		for (int i = 0; i < crews.size(); i++) {
+			System.out.println(crews.get(i));
+		}
+		
+		System.out.println("-------------------------");
+		
+		
+		
+//		
+//		마지막 버스를 구한다.
+//		정원 - 남아있는 인원이 1 이상이면 해당 버스의 시간
+//		정원 - 남아있는 인원이 1 미만이면?
+//		남아있는 인원의 정렬
+//		정원 - 1
+//		나를 포함한 인원
+			
+		// 탑승
+//		int myBus = 0;
+//		boolean earlybird = false;
+//		for (int i = 0; i < times.size(); i++) {
+//			int capacity = m;
+//			for (int j = 0; j < timetable.length; j++) {
+//				if (timetable[j] != null && timetable[j] != "" && (times.get(i).isAfter(LocalTime.parse(timetable[j])) || times.get(i).equals(LocalTime.parse(timetable[j])))  && capacity != 0) {
+//					timetable[j] = "";
+//					capacity--;
+//				}
+//			}
+//			myBus = i;
+//			if (capacity == 1) {
+//				;
+//			}
+//		}
+//		
+//		
+//		System.out.println(times.get(myBus));
+		
+		
+//		for (int i = 0; i < timetable.length; i++) {
+//			System.out.println(timetable[i]);
+//		}
+		
 	}
 	
 	// Number Check
